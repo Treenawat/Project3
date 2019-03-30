5a1f1ed3a6ed543ab0294e91794ac2749043c2f4
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-03-30 17:21:57 -0500

    Finish comparing mesonet with Fair

--- Project03/src/MesoEqual.java
+++ Project03/src/MesoEqual.java
@@ -41,28 +41,61 @@ public class MesoEqual {
 		letter[2] = StID.charAt(2);
 		letter[3] = StID.charAt(3);
 		
-		double mean = 0;
+		double StIDAverage = 0;
 		for (int i = 0; i < letter.length; i++) {
-			mean += letter[i];
+			StIDAverage += letter[i];
 		}
-		mean = mean/letter.length;
+		StIDAverage = StIDAverage/letter.length;
 		
-		String[] split = String.valueOf(mean).split("\\.");
+		String[] split = String.valueOf(StIDAverage).split("\\.");
 		int[] split2 = new int[2];
 		split2[0]=Integer.parseInt(split[0]);
 		split2[1]=Integer.parseInt(split[1]); 
 		int firstDigit = Integer.parseInt(Integer.toString(split2[1]).substring(0, 1));
 
-		int average = 0;
+		int StIDAscii = 0;
 		if (firstDigit >= 5) {
-			average = (int) Math.ceil(mean);
+			StIDAscii = (int) Math.ceil(StIDAverage);
 		}
 		else {
-			average = (int) Math.floor(mean);
+			StIDAscii = (int) Math.floor(StIDAverage);
 		}
 		
-		
-				
-		
+		//Add word with same Average from Mesonet with word "Fair" to hashmap sameAverage
+		for(int i = 0; i < mesonetArray.size(); ++i)
+		{
+			int[] letter2 = new int[4];
+			letter2[0] = StID.charAt(0);
+			letter2[1] = StID.charAt(1);
+			letter2[2] = StID.charAt(2);
+			letter2[3] = StID.charAt(3);
+			
+			//find average and set floor and ceiling
+			double mean = 0;
+			for (int j = 0; j < letter.length; j++) {
+				mean += letter2[i];
+			}
+			mean = mean/letter2.length;
+
+			String[] split3 = String.valueOf(mean).split("\\.");
+			int[] split4 = new int[2];
+			split4[0]=Integer.parseInt(split3[0]);
+			split4[1]=Integer.parseInt(split3[1]); 
+			int firstDecimal = Integer.parseInt(Integer.toString(split4[1]).substring(0, 1));
+			
+			int average = 0;
+			if (firstDecimal >= 5) {
+				average = (int) Math.ceil(mean);
+			}
+			else {
+				average = (int) Math.floor(mean);
+			}
+			
+			if(average == StIDAscii)
+			{
+				sameAverage.put(StID, average);
+			}
+		}
+	return sameAverage;
 	}
 }

faa384371a8dfce67707b731114abc9902dcc52c
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-03-30 17:13:27 -0500

    finish buffer reading and find average

--- Project03/src/MesoEqual.java
+++ Project03/src/MesoEqual.java
@@ -6,15 +6,40 @@ import java.io.File;
 import java.io.FileReader;
 
 public class MesoEqual {
-	String stID;
+	String StID;
 	
 	//this.
-	public MesoEqual(String stID) {
-		this.stID = stID;
+	public MesoEqual(String StID) {
+		this.StID = StID;
 	}
 	
 	//create hashmap according to main (calAsciiEqual)
 	public HashMap<String, Integer> calAsciiEqual() throws IOException{
+		//Find same average as Fair
+		HashMap<String, Integer> sameAverage = new HashMap<String, Integer>();
+		ArrayList<String> mesonetArray = new ArrayList<String>();
+		
+		//Read file from Mesonet and assign it as newWord, then loop it to add those newWord into mesonetArray
+		File file = new File("Mesonet.txt");
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String newWord;
+		newWord = br.readLine();
+		newWord = br.readLine();
+		newWord = br.readLine();
+		newWord = br.readLine();
+		newWord = br.readLine();
+		while(newWord != null) {
+			mesonetArray.add(newWord.substring(2,  6));
+			newWord = br.readLine();
+		}
+		br.close();
+		
+		//Use similar method from MesoAscii to find average
+		int[] letter = new int[4];
+		letter[0] = StID.charAt(0);
+		letter[1] = StID.charAt(1);
+		letter[2] = StID.charAt(2);
+		letter[3] = StID.charAt(3);
 		
 	}
 }

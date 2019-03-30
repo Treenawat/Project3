306d60c7aa4530aa8c3d4bd6155747843950e44a
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-03-30 17:14:15 -0500

    Finish finding average

--- Project03/src/MesoEqual.java
+++ Project03/src/MesoEqual.java
@@ -41,5 +41,28 @@ public class MesoEqual {
 		letter[2] = StID.charAt(2);
 		letter[3] = StID.charAt(3);
 		
+		double mean = 0;
+		for (int i = 0; i < letter.length; i++) {
+			mean += letter[i];
+		}
+		mean = mean/letter.length;
+		
+		String[] split = String.valueOf(mean).split("\\.");
+		int[] split2 = new int[2];
+		split2[0]=Integer.parseInt(split[0]);
+		split2[1]=Integer.parseInt(split[1]); 
+		int firstDigit = Integer.parseInt(Integer.toString(split2[1]).substring(0, 1));
+
+		int average = 0;
+		if (firstDigit >= 5) {
+			average = (int) Math.ceil(mean);
+		}
+		else {
+			average = (int) Math.floor(mean);
+		}
+		
+		
+				
+		
 	}
 }

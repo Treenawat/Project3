68f4633cb40e299666b6a338cec415f573622d45
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-03-30 17:27:35 -0500

    Related it to main first

--- Project03/src/StationLexicographical.java
+++ Project03/src/StationLexicographical.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Collections;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.TreeMap;
+
 public class StationLexicographical extends MesoSortedAbstract {
+	//create hashmap according to main (AsciiVal)
+	HashMap<String, Integer> AsciiVal;
 	
 }

a2a24400b966fc48ec4c07b307768db6bfb3961d
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-03-30 17:43:04 -0500

    Finish sorted

--- Project03/src/StationLexicographical.java
+++ Project03/src/StationLexicographical.java
@@ -20,4 +20,23 @@ public class StationLexicographical extends MesoSortedAbstract {
 		}
 	}
 	
+	@Override
+	Map<String, Integer> sortedMap(HashMap<String, Integer> unsortedMap) {
+		ArrayList<String> newWord = new ArrayList<String>();
+		Set<Map.Entry<String, Integer>> unSorted = AsciiVal.entrySet();
+		Iterator<Entry<String, Integer>> iterate = unSorted.iterator();
+		
+		for(int i = 0; i < unsortedMap.size(); i++){
+			newWord.add(iterate.next().getKey());
+		}
+		
+		Collections.sort(newWord);
+		Map<String, Integer> sorted = new TreeMap<String, Integer>();
+		for(int j = 0; j < newWord.size(); j++){
+			String stid = newWord.get(j);
+			Integer ascii = unsortedMap.get(stid);
+			sorted.put(stid, ascii);	
+		}
+		return sorted;
+	}
 }

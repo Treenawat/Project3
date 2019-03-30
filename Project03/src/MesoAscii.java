af18c1b1abcf92f7532ad167546a0f70f0ad092d
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-03-30 15:58:56 -0500

    Seperate character into an array in int form

--- Project03/src/MesoAscii.java
+++ Project03/src/MesoAscii.java
@@ -5,6 +5,16 @@ public class MesoAscii extends MesoAsciiAbstract {
 		this.StID = StID.getStID();
 	}
 	
+	@Override
+	int calAverage() {
+		//Create array for containing int valuable of each character
+		int[] letter = new int[4];
+		letter[0] = StID.charAt(0);
+		letter[1] = StID.charAt(1);
+		letter[2] = StID.charAt(2);
+		letter[3] = StID.charAt(3);
+		
+	}
 	
 	
 }

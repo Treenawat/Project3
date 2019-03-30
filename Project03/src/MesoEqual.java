3ab699b171a63983590a174da569b6b038f2bee7
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-03-30 16:35:52 -0500

    add string StID

--- Project03/src/MesoEqual.java
+++ Project03/src/MesoEqual.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.util.HashMap;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+
 public class MesoEqual {
+	String stID;
 	
 }
